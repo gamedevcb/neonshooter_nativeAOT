ef33b352731b325e610e302b21d343873a8ab495
gamedevcb <76231490+gamedevcb@users.noreply.github.com>
2021-03-10 23:11:15 +0000

    Add files via upload

---
+++ MyGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NeonShooter;
+namespace MyGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new NeonShooterGame())
+                game.Run();
+        }
+    }
+}
